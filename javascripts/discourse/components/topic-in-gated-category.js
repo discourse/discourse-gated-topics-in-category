4c2afc298449ee6d6d230c6f4060d3933129bbcf
chapoi <charlie@discourse.org>
2022-09-01 20:47:12 +0200

    add tags + minor css change

--- javascripts/discourse/components/topic-in-gated-category.js
+++ javascripts/discourse/components/topic-in-gated-category.js
@@ -6,9 +6,12 @@ const enabledCategories = settings.enabled_categories
   .map((id) => parseInt(id, 10))
   .filter((id) => id);
 
+const enabledTags = settings.enabled_tags.split("|");
+
 export default Component.extend({
   tagName: "",
   hidden: true,
+  gatedByTag: false,
 
   didInsertElement() {
     this._super(...arguments);
@@ -26,18 +29,20 @@ export default Component.extend({
 
   recalculate() {
     // do nothing if:
-    // a) topic does not have a category
+    // a) topic does not have a category and does not have a gated tag
     // b) component setting is empty
     // c) user is logged in
+    this.gatedByTag = this.tags.some((t) => enabledTags.indexOf(t) >= 0);
+
     if (
-      !this.categoryId ||
-      enabledCategories.length === 0 ||
+      (!this.categoryId && !this.gatedByTag) ||
+      (enabledCategories.length === 0 && enabledTags.length === 0) ||
       this.currentUser
     ) {
       return;
     }
 
-    if (enabledCategories.includes(this.categoryId)) {
+    if (enabledCategories.includes(this.categoryId) || this.gatedByTag) {
       document.body.classList.add("topic-in-gated-category");
       this.set("hidden", false);
     }
